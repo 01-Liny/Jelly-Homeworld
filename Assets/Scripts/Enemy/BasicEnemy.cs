a892bedda43765318f49b57473e4cc194ae78744
XSAM <XSAM@github.com>
2016-05-05 11:39:45 +0800

    Add enemy.implements tower mutiply fire.

---
+++ Assets/Scripts/Enemy/BasicEnemy.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class BasicEnemy : MonoBehaviour 
+{
+    public bool GoDie = false;
+    [SerializeField]protected float health = 100;
+
+    private bool isDied = false;
+    private new Collider collider;
+	private List<Collider> towerTriggerList = new List<Collider>();
+    // Use this for initialization
+    void Start () 
+	{
+        collider=GetComponent<Collider>();
+    }
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if(GoDie==true)
+		{
+            Died();
+            GoDie = false;
+        }
+		if(health<=0&&isDied==false)
+		{
+            isDied = true;
+            Died();
+        }
+	}
+	
+	//敌人死后
+	protected void Died()
+	{
+		//防止在敌人死亡的过程中出现在新的塔的范围内
+        collider.enabled = false;
+		
+        for(int i=0;i<towerTriggerList.Count;i++)
+		{
+			//通过塔的collider找到BasicTower脚本，调用RemoveMyselfFromEnemyList函数将敌人从列表中移除
+            towerTriggerList[i].GetComponent<BasicTower>().RemoveMyselfFromEnemyList(collider);
+        }
+        Debug.Log("Died");
+		//死亡后自我销毁
+        Destroy(this.gameObject);
+    }
+	
+	public void TakeDamage(float fireDamage)
+	{
+        health -= fireDamage;
+        Debug.Log("Health:" + health);
+    }
+	
+	//敌人出现在塔的攻击范围内，加入到塔列表中
+    protected void OnTriggerEnter(Collider other)
+    {
+        if(!towerTriggerList.Contains(other))
+        {
+            towerTriggerList.Add(other);
+        }
+        Debug.Log("Tower Enter");
+    }
+    
+    //敌人离开塔的攻击范围，将塔从塔列表中移除
+    protected void OnTriggerExit(Collider other)
+    {
+        if(towerTriggerList.Contains(other))
+        {
+            towerTriggerList.Remove(other);
+        }
+        Debug.Log("Tower Exit");
+    }
+	
+}
--- Assets/Scripts/Towers/BasicTower.cs
+++ Assets/Scripts/Towers/BasicTower.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class BasicTower : MonoBehaviour 
 {
@@ -14,6 +15,7 @@ public class BasicTower : MonoBehaviour
     protected SphereCollider attackRangeCollider;//攻击范围collier
     
     private float nextFire;//下次攻击的时间
+    private List<Collider> enemyTriggerList = new List<Collider>();
 
     // Use this for initialization
     protected void Start () 
@@ -23,21 +25,46 @@ public class BasicTower : MonoBehaviour
         RecalcInfo();
     }
     
-    //敌人出现在塔的攻击范围内
-    protected void OnTriggerStay(Collider other)
+    protected void Update()
     {
-        if (other.gameObject.tag == "Enemy")
+        //当敌人列表不为空时，尝试攻击
+        if(enemyTriggerList.Count>0)
         {
-            Fire();
+            TryFire();
         }
     }
     
-    protected void Fire()
+    //敌人出现在塔的攻击范围内，加入到敌人列表中
+    protected void OnTriggerEnter(Collider other)
+    {
+        if(!enemyTriggerList.Contains(other))
+        {
+            enemyTriggerList.Add(other);
+        }
+        Debug.Log("Enter");
+    }
+    
+    //敌人离开塔的攻击范围，将敌人从敌人列表中移除
+    protected void OnTriggerExit(Collider other)
+    {
+        if(enemyTriggerList.Contains(other))
+        {
+            enemyTriggerList.Remove(other);
+        }
+        Debug.Log("Exit");
+    }
+    
+    //攻击
+    protected void TryFire()
     {
         if (Time.time > nextFire)//当前时间超过下次攻击时间，表示可以开始攻击
         {
             nextFire = Time.time + fireRate;//计算下一次攻击时间
-            Debug.Log("FireDamge:"+this.realFireDamage);
+            for(int i=0;i<enemyTriggerList.Count;i++)
+            {
+                Debug.Log("Attack");
+                enemyTriggerList[i].GetComponent<BasicEnemy>().TakeDamage(realFireDamage);         
+            }
         }
     }
     
@@ -68,5 +95,14 @@ public class BasicTower : MonoBehaviour
         this.buffFireRate += buffFireRate;
     }
     
-
+    //用于敌人死亡时调用，将敌人从检测到敌人的塔的敌人列表中移除。防止塔攻击死亡敌人
+    public void RemoveMyselfFromEnemyList(Collider other)
+    {
+        Debug.Log(enemyTriggerList.Count);
+        if(enemyTriggerList.Contains(other))
+        {
+            enemyTriggerList.Remove(other);
+        }
+        Debug.Log(enemyTriggerList.Count);
+    }
 }
