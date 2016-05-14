255142afb2e6200fb00efd246510e35a5392f676
XSAM <xbs2468@gmail.com>
2016-05-14 14:19:45 +0800

    Add StrikeTower

---
+++ Assets/Scripts/Towers/StrikeTower.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StrikeTower : BasicTower 
+{
+    [SerializeField]protected float strikeArmor;//减甲
+
+    public override void Init(TowerLevel towerLevel)
+    {
+        towerType = TowerType.Strike;
+        base.Init(towerLevel);
+    }
+
+    protected override void RereadTowerInfo()
+    {
+        base.RereadTowerInfo();
+        strikeArmor = TowerInfo.strikeArmor[(int)towerType, (int)towerLevel];//实际不用这些数据
+    }
+
+    protected override void Fire()
+    {
+        basicEnemyMinHealth.TakeDamage(realFireDamage,1f,towerLevel,TowerLevel.Empty);
+    }
+}
