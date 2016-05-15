cc28344e7a3a7c01b3c97c7183a370d1b88a8945
XSAM <XSAM@github.com>
2016-05-15 22:10:45 +0800

    Add UISelectedArea scipt.Add Frame prefab

---
+++ Assets/Scripts/UI/UISelectedArea.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UISelectedArea : MonoBehaviour 
+{
+	public GameObject m_Prefabs;
+    public Camera m_Camera;
+    public RaycastHit m_Hit;
+    
+    private Vector3 m_VecTemp=new Vector3();
+
+    public void Update()
+    {
+        //从摄像头的坐标到鼠标在屏幕上的坐标的方向向量
+        Ray ray = m_Camera.ScreenPointToRay(Input.mousePosition);
+        
+        //以ray的方向和原点，发射一条射线，检测射线是否碰撞到LayerMask为Plane的Collider
+        if(Physics.Raycast(ray.origin, ray.direction, out m_Hit, Mathf.Infinity, 1 << 8))
+        {
+            m_VecTemp = m_Hit.point;
+            m_VecTemp.y = 100;
+            Debug.DrawLine(m_Hit.point, m_VecTemp, Color.blue);
+            Debug.DrawRay(ray.origin, ray.direction * 20, Color.yellow);
+        }
+        //给选中框做位置偏移
+        m_VecTemp.x=(int)(m_Hit.point.x+0.5f*Mathf.Sign(m_Hit.point.x));
+        m_VecTemp.z=(int)(m_Hit.point.z+0.5f*Mathf.Sign(m_Hit.point.z));
+        m_VecTemp.y = 0.1f;
+
+        m_Prefabs.transform.position = m_VecTemp;
+    }
+}
