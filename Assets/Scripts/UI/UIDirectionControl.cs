8514dd78fdfad444c7e6e8fa82b3afcce768a493
XSAM <XSAM@github.com>
2016-05-17 20:09:32 +0800

    Fixed UIHealth rotation problem.

--- Assets/Scripts/UI/UIDirectionControl.cs
+++ Assets/Scripts/UI/UIDirectionControl.cs
@@ -3,8 +3,13 @@ using System.Collections;
 
 public class UIDirectionControl : MonoBehaviour 
 {
+    Quaternion m_ParentRotation;
+    private void Start()
+    {
+        m_ParentRotation = transform.parent.rotation;
+    }
     private void Update()
     {
-        transform.rotation = Quaternion.identity;
+        transform.rotation = m_ParentRotation;
     }
 }
