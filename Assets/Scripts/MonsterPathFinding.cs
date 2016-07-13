1e1a247d6c7ebe4fa21cd389fe11e2bd5a0766b0
XSAM <xbs2468@gmail.com>
2016-07-14 07:43:27 +0800

    Modify File.Merge Branch Completed.

---
+++ Assets/Scripts/MonsterPathFinding.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;//使用泛型的时候需要这个
+
+public class MonsterPathFinding : MonoBehaviour
+{
+    private int currentWayPoint = 0;
+    private float speed = 1.0f;
+
+    private static int m = 5, n = 5;			//定义地图的高和宽
+    private int shortestPath = 1000;
+    public static int stackSize;				//最短路径的数组大小（最小路径的栈的大小）
+    public static int ListSize = 0;
+    public static int[,] moveArray = new int[8, 2] { { 1, 1 }, { 1, -1 }, { -1, 1 }, { -1, -1 }, { 0, 1 }, { 1, 0 }, { 0, -1 }, { -1, 0 } };
+
+
+    public class Node : ICloneable
+    {
+        public int x;
+        public int y;
+        public int pathLength;
+        public int[,] markMapArray = new int[50, 50];
+
+        public Node() { }
+
+        public Node(int[,] mapArray)
+        {
+            for (int i = 0; i < m + 2; i++)
+            {
+                for (int j = 0; j < n + 2; j++)
+                {
+                    markMapArray[i, j] = mapArray[i, j];
+                }
+            }
+        }
+        //浅拷贝
+        public object Clone()
+        {
+            return this.MemberwiseClone();
+        }
+        //深拷贝
+        public object DeepClone()
+        {
+            Node m_Temp = new Node();
+            m_Temp.x = this.x;
+            m_Temp.y = this.y;
+            m_Temp.pathLength = this.pathLength;
+            for (int i = 0; i < m + 2; i++)
+            {
+                for (int j = 0; j < n + 2; j++)
+                {
+                    m_Temp.markMapArray[i, j] = this.markMapArray[i, j];
+                }
+            }
+            return m_Temp;
+        }
+        public bool whetherMove()
+        {
+            int tempX, tempY;
+            for (int i = 0; i < 7; i++)
+            {
+                tempX = x + moveArray[i, 0];
+                tempY = y + moveArray[i, 1];
+                if (markMapArray[tempX, tempY] == 0)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public bool arriveEndPosition(int endX, int endY)
+        {
+            if (x == endX && y == endY)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public void move()
+        {
+            int tempX, tempY;
+            for (int i = 0; i < 7; i++)
+            {
+                tempX = x + moveArray[i, 0];
+                tempY = y + moveArray[i, 1];
+                if (markMapArray[tempX, tempY] == 0)
+                {
+                    markMapArray[tempX, tempY] = 1;
+                    x = tempX;
+                    y = tempY;
+                    if (i < 4)
+                    {
+                        pathLength += 3;
+                    }
+                    else
+                    {
+                        pathLength += 2;
+                    }
+                    return;
+                    //return tempX*m + tempY;//如果mod完m之后等于0，tempY=m，tempX=（返回值-m）/m
+                }
+            }
+        }
+
+        public void copyMarkMapArray(int[,] markMapArray)
+        {
+            for (int i = 0; i < m + 2; i++)
+            {
+                for (int j = 0; j < n + 2; j++)
+                {
+                    this.markMapArray[i, j] = markMapArray[i, j];
+                }
+            }
+        }
+    }   
+
+    public static Node[] m_pathArray = new Node[100];
+
+    private Node m_firstNode, m_tempNode;
+    private Stack<Node> m_pathStore;
+    public static List<Node> m_ListPath;
+
+    public void monsterPathFinding(int[,] mapArray, int starX, int starY, int endX, int endY)//寻路函数
+    {
+        bool findPath = false;
+        m_pathStore = new Stack<Node>();//bfs过程中存储路径的栈
+
+        //stack<Node> m_pathStore;//bfs过程中存储路径的栈
+        //stack<Node> *m_tempStore;//bfs过程中存放最优路径的栈
+
+        m_firstNode = new Node(mapArray);
+        m_tempNode = new Node(mapArray);
+
+        m_firstNode.x = starX;
+        m_firstNode.y = starY;
+        m_firstNode.markMapArray[starX, starY] = 1;
+        m_firstNode.pathLength = 0;
+
+        m_pathStore.Push(m_firstNode);
+        //m_ListPath.Add(m_firstNode);
+        //ListSize++;
+        while (m_pathStore.Count != 0)
+        {
+            m_tempNode = (Node)(m_pathStore.Peek().DeepClone());
+            //m_tempNode = (Node)m_ListPath[ListSize].DeepClone();
+            if (m_tempNode.whetherMove())
+            {
+                m_tempNode.move();
+                m_pathStore.Peek().copyMarkMapArray(m_tempNode.markMapArray);
+
+                //......优化当pathLength等于他们之间斜线的距离时，那就直接跳出
+
+                m_pathStore.Push(m_tempNode);
+                if (m_tempNode.arriveEndPosition(endX, endY))
+                {
+                    findPath = true;
+                    if (m_tempNode.pathLength < shortestPath)
+                    {
+                        //记下最短的路径长度
+                        shortestPath = m_tempNode.pathLength;
+                        stackSize = m_pathStore.Count;
+
+                        //方法一、
+                        m_pathStore.CopyTo(m_pathArray, 0);//把栈里元素copy入数组，从数组0开始copy
+                        m_ListPath = new List<Node>(m_pathStore.ToArray());
+                        //方法二、
+                        //存储下pathStore里的所有坐标
+                        //m_tempStore = new Stack<Node>(m_pathStore);//用已有的stack再new一个stack时两个stack栈里的元素是相反的
+                        //for (int j = m_tempStore.Count - 1; j >= 0; j--)
+                        //{
+                        //    m_pathArray[j] = m_tempStore.Peek();
+                        //    m_tempStore.Pop();
+                        //}
+                        //for (int j = 0; j < stackSize; j++)//这里不能用j < m_tempStore.Count,因为m_tempStore.Count的个数一直在变（好...蠢）
+                        //{
+                        //    m_pathArray[j] = (Node)m_tempStore.Peek().DeepClone();
+                        //    m_tempStore.Pop();
+                        //}
+                    }
+                }
+            }
+            else
+            {
+                m_pathStore.Pop();
+            }
+
+        }
+    }
+ 
+}
---
+++ Assets/Scripts/MonsterWalk.cs
@@ -0,0 +1,113 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;//使用泛型的时候需要这个
+
+public class MonsterWalk : MonoBehaviour
+{
+    private Vector3 temp1, temp2, startPosition, endPosition, newDirection;
+    private Quaternion rotation;
+    private float pathLength, totalTimeForPath, currentTimeOnPath;
+    private Rigidbody rb;
+    private int currentWayPoint = 0;
+    private float lastWaypointSwitchTime;
+    private float speed = 1.0f;
+    private MonsterPathFinding.Node[] m_pathArray;
+    private List<MonsterPathFinding.Node> m_ListPath;
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        temp1 = new Vector3();
+        temp2 = new Vector3();
+        startPosition = new Vector3();
+        endPosition = new Vector3();
+        //for (int i = stackSize-1; i >=0; i--)
+        //{
+        //    Debug.Log(m_pathArray[i].x);
+        //    Debug.Log(m_pathArray[i].y);
+        //}
+
+        lastWaypointSwitchTime = Time.time;
+        m_pathArray = MonsterPathFinding.m_pathArray;
+        m_ListPath = MonsterPathFinding.m_ListPath;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //if (m_pathArray[0]!= null)
+        //{
+        //    for (int i = 0; i < MonsterPathFinding.stackSize - 1; i++)
+        //    {
+        //        temp1.Set(m_pathArray[i].x, 1, m_pathArray[i].y);
+        //        temp2.Set(m_pathArray[i + 1].x, 1, m_pathArray[i + 1].y);
+        //        Debug.DrawLine(temp1, temp2, Color.blue);
+        //    }
+        //    startPosition.Set(m_pathArray[currentWayPoint].x, 1, m_pathArray[currentWayPoint].y);
+        //    endPosition.Set(m_pathArray[currentWayPoint + 1].x, 1, m_pathArray[currentWayPoint + 1].y);
+        //    newDirection = (endPosition - startPosition);
+
+        //    rotation = Quaternion.LookRotation(newDirection);
+        //    transform.rotation = Quaternion.Lerp(transform.rotation, rotation, 0.25f);
+        //    //gameObject.transform.(endPosition);
+        //    //gameObject.transform.Translate(Vector3.forward);
+
+        //    //float x = newDirection.x;
+        //    //float y = newDirection.y;
+        //    //float rotationAngle = Mathf.Atan2(y, x) * 180 / Mathf.PI;
+        //    //transform.rotation = Quaternion.AngleAxis(rotationAngle, Vector3.forward);
+
+        //    pathLength = Vector3.Distance(startPosition, endPosition);
+        //    totalTimeForPath = pathLength / speed;//路径上总共需要花费的时间
+        //    currentTimeOnPath = Time.time - lastWaypointSwitchTime;
+        //    transform.position = Vector3.Lerp(startPosition, endPosition, currentTimeOnPath / totalTimeForPath);
+        //    //RotateIntoMoveDirection();
+        //    if (transform.position == endPosition)
+        //    {
+        //        if (currentWayPoint < MonsterPathFinding.stackSize - 2)
+        //        {
+        //            currentWayPoint++;
+        //            lastWaypointSwitchTime = Time.time;
+        //            // TODO: Rotate into move direction
+        //        }
+        //    }
+        //}
+        if (m_ListPath[0] != null)
+        {
+            for (int i = 0; i < MonsterPathFinding.stackSize - 1; i++)
+            {
+                temp1.Set(m_ListPath[i].x, 1, m_ListPath[i].y);
+                temp2.Set(m_ListPath[i + 1].x, 1, m_ListPath[i + 1].y);
+                Debug.DrawLine(temp1, temp2, Color.blue);
+            }
+            startPosition.Set(m_ListPath[currentWayPoint].x, 1, m_ListPath[currentWayPoint].y);
+            endPosition.Set(m_ListPath[currentWayPoint + 1].x, 1, m_ListPath[currentWayPoint + 1].y);
+            newDirection = (endPosition - startPosition);
+
+            rotation = Quaternion.LookRotation(newDirection);
+            transform.rotation = Quaternion.Lerp(transform.rotation, rotation, 0.25f);
+            //gameObject.transform.(endPosition);
+            //gameObject.transform.Translate(Vector3.forward);
+
+            //float x = newDirection.x;
+            //float y = newDirection.y;
+            //float rotationAngle = Mathf.Atan2(y, x) * 180 / Mathf.PI;
+            //transform.rotation = Quaternion.AngleAxis(rotationAngle, Vector3.forward);
+
+            pathLength = Vector3.Distance(startPosition, endPosition);
+            totalTimeForPath = pathLength / speed;//路径上总共需要花费的时间
+            currentTimeOnPath = Time.time - lastWaypointSwitchTime;
+            transform.position = Vector3.Lerp(startPosition, endPosition, currentTimeOnPath / totalTimeForPath);
+            //RotateIntoMoveDirection();
+            if (transform.position == endPosition)
+            {
+                if (currentWayPoint < MonsterPathFinding.stackSize - 2)
+                {
+                    currentWayPoint++;
+                    lastWaypointSwitchTime = Time.time;
+                    // TODO: Rotate into move direction
+                }
+            }
+        }
+    }
+}
