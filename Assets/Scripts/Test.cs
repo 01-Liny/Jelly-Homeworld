06d9a5675ea2b89a704ba10de97b67f1e48996cc
XSAM <xbs2468@gmail.com>
2016-10-14 07:43:21 +0800

    Upgrade Touch Gesture

--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -1,39 +1,104 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
+using TouchScript.Gestures;
+using UnityEngine.EventSystems;
 
 public class Test : MonoBehaviour 
 {
-    [Range(0, 100)]
-    public float health;
+
+    public FSM m_FSM;//总状态机
+    public UISelectedArea m_SelectedArea;
+    public Camera m_Camera;
+
+    public TapGesture m_TapGesture;
+    public float PanSpeed = 200f;
+    public float ZoomSpeed = 10f;
+
+    private bool isMoved = false;
+
+    private bool isTouched = false;
+    private int firstTouchId;
+
+    private Vector3 m_CameraOriginPos;
+    private Vector2 m_Origin1;
+
+    private Vector2 m_Drection1;
+    private Vector2 m_Origin2;
+    private Vector2 m_Drection2;
+
+
+    private float distance;
+
+    private bool isSingle = false;
+
+
+    private Ray ray;
+    private RaycastHit m_Hit;
+    private Vector2 m_HitPos;
 
     private float screenOffsetWidth;
     private float screenOffsetHeight;
-    public Text text;
-
-    private Image m_Image;
-    private Color m_Color;
 
     private void Start()
     {
-        //m_Image = GetComponent<Image>();
-        //m_Color = new Color();
-        //Vector3 t1 = new Vector3(1, 2, 3);
-        //Vector2 t2 = (Vector2)t1;
-
-        screenOffsetWidth = Screen.width;
-        screenOffsetHeight = Screen.height;
-        //text.GetComponent<Text>();
-
-        text.text += screenOffsetWidth + " , " + screenOffsetHeight + " , " + Screen.dpi;
+        //pc上的摄像头视角大小不一样，注释值为pc上的数值
+        screenOffsetWidth = Screen.width / 1.87f;//3.55
+        screenOffsetHeight = Screen.height / 2.9f;//2
     }
 
-    private void Update()
+    private void OnEnable()
     {
-        //m_Color = m_Image.color;
-        //m_Color.r = (100-health) / 100.0f;
-        //m_Color.g = health / 100.0f;
-        //m_Color.b = 0;
-        //m_Image.color = m_Color;
+
+        m_TapGesture.Tapped += TappedHandler;
     }
+
+    private void OnDisable()
+    {
+        m_TapGesture.Tapped -= TappedHandler;
+    }
+
+    private void TappedHandler(object sender, System.EventArgs e)
+    {
+        if (isMoved)
+        {
+            isMoved = false;
+            return;
+        }
+        if (EventSystem.current.IsPointerOverGameObject(Input.GetTouch(0).fingerId))
+            return;
+        m_FSM.OnClick();
+    }
+    //[Range(0, 100)]
+    //public float health;
+
+    //private float screenOffsetWidth;
+    //private float screenOffsetHeight;
+    //public Text text;
+
+    //private Image m_Image;
+    //private Color m_Color;
+
+    //private void Start()
+    //{
+    //    //m_Image = GetComponent<Image>();
+    //    //m_Color = new Color();
+    //    //Vector3 t1 = new Vector3(1, 2, 3);
+    //    //Vector2 t2 = (Vector2)t1;
+
+    //    screenOffsetWidth = Screen.width;
+    //    screenOffsetHeight = Screen.height;
+    //    //text.GetComponent<Text>();
+
+    //    text.text += screenOffsetWidth + " , " + screenOffsetHeight + " , " + Screen.dpi;
+    //}
+
+    //private void Update()
+    //{
+    //    //m_Color = m_Image.color;
+    //    //m_Color.r = (100-health) / 100.0f;
+    //    //m_Color.g = health / 100.0f;
+    //    //m_Color.b = 0;
+    //    //m_Image.color = m_Color;
+    //}
 }
---
+++ Assets/TouchScript/Examples/Camera/Scripts/CameraController.cs
@@ -0,0 +1,52 @@
+﻿/*
+ * @author Valentin Simonov / http://va.lent.in/
+ */
+
+using UnityEngine;
+using TouchScript.Gestures;
+
+namespace TouchScript.Examples.CameraControl
+{
+    public class CameraController : MonoBehaviour
+    {
+        public ScreenTransformGesture TwoFingerMoveGesture;
+        public ScreenTransformGesture ManipulationGesture;
+        public float PanSpeed = 200f;
+        public float RotationSpeed = 200f;
+        public float ZoomSpeed = 10f;
+
+        private Transform pivot;
+        private Transform cam;
+
+        private Camera m_Camera;
+
+        private void Awake()
+        {
+            pivot = transform.Find("Pivot");
+            cam = transform.Find("Pivot/Camera");
+            m_Camera = cam.GetComponent<Camera>();
+        }
+
+        private void OnEnable()
+        {
+            TwoFingerMoveGesture.Transformed += twoFingerTransformHandler;
+            ManipulationGesture.Transformed += manipulationTransformedHandler;
+        }
+
+        private void OnDisable()
+        {
+            TwoFingerMoveGesture.Transformed -= twoFingerTransformHandler;
+            ManipulationGesture.Transformed -= manipulationTransformedHandler;
+        }
+
+        private void manipulationTransformedHandler(object sender, System.EventArgs e)
+        {
+            m_Camera.orthographicSize -= (ManipulationGesture.DeltaScale - 1f) * ZoomSpeed;
+        }
+
+        private void twoFingerTransformHandler(object sender, System.EventArgs e)
+        {
+            pivot.localPosition += pivot.rotation * TwoFingerMoveGesture.DeltaPosition * PanSpeed;
+        }
+    }
+}
